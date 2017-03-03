ceec9118d2324ff73608b2ab3b9e4b917a77c38e
Marie-Maxime Tanguay <mariemaximetanguay@gmail.com>
2017-03-02 21:23:32 -0500

    ajout de commentaires

--- TanguayMarieMaxime_ex9.js
+++ TanguayMarieMaxime_ex9.js
@@ -2,12 +2,14 @@ const express = require('express');
 const bodyParser= require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const app = express();
+
 app.set('view engine', 'ejs'); // générateur de template «ejs»
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'))  // pour utiliser le dossier public
 
 var db // variable qui contiendra le lien sur la BD
 
+//pour se connecter à la base de donnée Mongo
 MongoClient.connect('mongodb://127.0.0.1:27017/carnet_adresse', (err, database) => {
   if (err) return console.log(err)
   db = database
@@ -17,7 +19,7 @@ MongoClient.connect('mongodb://127.0.0.1:27017/carnet_adresse', (err, database) 
 })
 
 
-
+//acces au fichier index
 app.get('/',  (req, res) => {
    console.log('la route route get / = ' + req.url)
 
@@ -30,17 +32,22 @@ app.get('/',  (req, res) => {
     })
 })
 
+//pour avoir acces au dossier public et le css
 app.use(express.static(__dirname + "/public"));
 
+//avoir acces au formulaire
 app.get('/formulaire',  (req, res) => {
    console.log('la route  get / = ' + req.url)
    res.sendFile(__dirname + "/public/html/forme.htm")
 })
 
+//requete post lorsque le formulaire est submit
 app.post('/adresse',  (req, res) => {
+  //on sauvegarde les données dans la DB mongo
   db.collection('adresse').save(req.body, (err, result) => {
       if (err) return console.log(err)
       console.log('sauvegarder dans la BD')
+      //redirige vers /
       res.redirect('/')
     })
 })
