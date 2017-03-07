6751f2c5e6d52aed2ac6add37d63bcac13f4095c
Marie-Maxime Tanguay <mariemaximetanguay@gmail.com>
2017-03-07 16:38:55 -0500

    ajout de la fonction modifier

--- TanguayMarieMaxime_ex10.js
+++ TanguayMarieMaxime_ex10.js
@@ -1,65 +1,95 @@
 const express = require('express');
-const bodyParser= require('body-parser')
+const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const app = express();
 const ObjectID = require('mongodb').ObjectID;
+// variable qui contiendra le lien sur la BD
+let db;
 
 app.set('view engine', 'ejs'); // générateur de template «ejs»
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static('public'))  // pour utiliser le dossier public
 
-app.post('/detruire/:id', (req, res) => {
- var id = req.params.id
- console.log(id)
- db.collection('adresse')
- .findOneAndDelete({"_id": ObjectID(req.params.id)}, (err, resultat) => {
+// parse application/x-www-form-urlencoded
+let urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-if (err) return console.log(err)
- res.redirect('/')  // redirige vers la route qui affiche la collection
- })
-})
-
-var db // variable qui contiendra le lien sur la BD
-
-//pour se connecter à la base de donnée Mongo
-MongoClient.connect('mongodb://127.0.0.1:27017/carnet_adresse', (err, database) => {
-  if (err) return console.log(err)
-  db = database
-  app.listen(8081, () => {
-    console.log('connexion à la BD et on écoute sur le port 8081')
-  })
-})
-
-
-//acces au fichier index
-app.get('/',  (req, res) => {
-   console.log('la route route get / = ' + req.url)
-
-    var cursor = db.collection('adresse').find().toArray(function(err, resultat){
-       if (err) return console.log(err)
-    // renders index.ejs
-    // affiche le contenu de la BD
-    res.render('index.ejs', {adresse: resultat})
-
-    })
-})
+// parse application/json
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 //pour avoir acces au dossier public et le css
 app.use(express.static(__dirname + "/public"));
 
+app.post('/detruire/:id', (req, res) => {
+    const id = req.params.id
+    console.log(id)
+    db.collection('adresse').findOneAndDelete({
+        "_id": ObjectID(req.params.id)
+    }, (err, resultat) => {
+        if (err)
+            return console.log(err)
+        res.redirect('/') // redirige vers la route qui affiche la collection
+    })
+})
+
+app.post("/modifier", urlencodedParser,(req, res) => {
+    const id = req.body._id;
+    const data = req.body;
+    const lesDonnees = {
+        "nom": data.nom,
+        "prenom": data.prenom,
+        "telephone":data.telephone,
+        "ville":data.ville,
+        "postal":data.postal
+    }
+    console.log(data);
+    db.collection('adresse').update({
+        "_id": ObjectID(id)
+    }, lesDonnees , (err, resultat) => {
+        if (err)
+            return console.log(err)
+        res.redirect('/') // redirige vers la route qui affiche la collection
+    });
+});
+
+//pour se connecter à la base de donnée Mongo
+MongoClient.connect('mongodb://127.0.0.1:27017/carnet_adresse', (err, database) => {
+    if (err)
+        return console.log(err)
+    db = database
+    app.listen(8081, () => {
+        console.log('connexion à la BD et on écoute sur le port 8081')
+    })
+})
+
+//acces au fichier index
+app.get('/', (req, res) => {
+    console.log('la route route get / = ' + req.url)
+
+    var cursor = db.collection('adresse').find().toArray(function(err, resultat) {
+        if (err)
+            return console.log(err)
+            // renders index.ejs
+        // affiche le contenu de la BD
+        res.render('index.ejs', {adresse: resultat})
+
+    })
+})
+
+
+
 //avoir acces au formulaire
-app.get('/formulaire',  (req, res) => {
-   console.log('la route  get / = ' + req.url)
-   res.sendFile(__dirname + "/public/html/forme.htm")
+app.get('/formulaire', (req, res) => {
+    console.log('la route  get / = ' + req.url)
+    res.sendFile(__dirname + "/public/html/forme.htm")
 })
 
 //requete post lorsque le formulaire est submit
-app.post('/adresse',  (req, res) => {
-  //on sauvegarde les données dans la DB mongo
-  db.collection('adresse').save(req.body, (err, result) => {
-      if (err) return console.log(err)
-      console.log('sauvegarder dans la BD')
-      //redirige vers /
-      res.redirect('/')
+app.post('/adresse', (req, res) => {
+    //on sauvegarde les données dans la DB mongo
+    db.collection('adresse').save(req.body, (err, result) => {
+        if (err)
+            return console.log(err)
+        console.log('sauvegarder dans la BD')
+        //redirige vers /
+        res.redirect('/')
     })
 })
--- public/js/modifier_adresse.js
+++ public/js/modifier_adresse.js
@@ -1,27 +1,39 @@
-xhr = new XMLHttpRequest();
-xhr.open('POST', "modifier", true);
-data = {
-    "modif": {
-        "nom": elmLigneDiv[0].innerHTML,
-        "prenom": elmLigneDiv[1].innerHTML,
-        "telephone": elmLigneDiv[2].innerHTML
-    },
-    "_id": elmLigneDiv[3].innerHTML
+var lesBoutons = document.getElementsByClassName("bouton-modifier");
+
+for (var i = 0; i < lesBoutons.length; i++) {
+    lesBoutons[i].addEventListener("click", modifierAdresse, false);
 }
 
-sData = JSON.stringify(data);
-xhr.setRequestHeader('Content-type', 'application/json');
-xhr.send(sData);
-xhr.addEventListener("readystatechange", traiterRequest, false);
+function modifierAdresse(){
+    var lesDonnees = this.parentNode.getElementsByTagName('span');
+    console.log(this.dataset.id);
+    var data = {
+        "_id": this.dataset.id,
+        "nom": lesDonnees[0].innerText,
+        "prenom": lesDonnees[1].innerText,
+        "telephone": lesDonnees[2].innerText,
+        "ville": lesDonnees[3].innerText,
+        "postal": lesDonnees[4].innerText
+    }
+    requeteXMLHttpRequest(data);
+
+}
+
+function requeteXMLHttpRequest(data){
+    xhr = new XMLHttpRequest();
+    xhr.open('POST', "modifier/", true);
+    sData = JSON.stringify(data);
+    xhr.setRequestHeader('Content-type', 'application/json');
+    xhr.send(sData);
+    xhr.addEventListener("readystatechange", traiterRequest, false);
+}
+
 
 function traiterRequest(e) {
     console.log("xhr.readyState = " + xhr.readyState)
     console.log("xhr.status = " + xhr.status)
     if (xhr.readyState == 4 && xhr.status == 200) {
         console.log('ajax fonctionne')
-        var response = JSON.parse(xhr.responseText);
         console.log(xhr.responseText);
-        elmChamp_id.innerHTML = response[0]._id
-        elmLigne.style.backgroundColor = "#0f0"
     }
 }
